da9e01b6e99cc053caca3021914b582fce9783a3
Sigrun Olafsdottir <solafsdottir@gmail.com>
2020-04-16 10:36:01 +0200

    Initial commit

---
+++ src/demos/HolaAmigoServlet.java
@@ -0,0 +1,29 @@
+package demos;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HolaAmigoServlet", urlPatterns={"/holaAmigos"})
+
+    public class HolaAmigoServlet extends HttpServlet {
+        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            String firstname = request.getParameter("fname");
+            String lastname = request.getParameter("lname");
+
+            PrintWriter p = response.getWriter();
+            p.println("<h1>Hello "+firstname+" "+lastname+"</h1>");
+        }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String firstname = request.getParameter("fname");
+        String lastname = request.getParameter("lname");
+
+        PrintWriter p = response.getWriter();
+        p.println("<h1>Hello "+firstname+" "+lastname+"</h1>");
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: s_ola
+  Date: 2020-04-16
+  Time: 10:04
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>A small form</title>
+  </head>
+  <body>
+  <form action="holaAmigos">
+    <label for="fname">First name:</label><br>
+    <input type="text" id="fname" name="fname" value="John"><br>
+    <label for="lname">Last name:</label><br>
+    <input type="text" id="lname" name="lname" value="Doe"><br><br>
+    <input type="submit" value="Submit">
+  </form>
+  </body>
+</html>
